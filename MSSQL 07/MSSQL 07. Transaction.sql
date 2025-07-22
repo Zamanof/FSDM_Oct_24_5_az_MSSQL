fceb7c61b49a09fd035c6ed3b4deee76b26536a9
Nadir Zamanov <Zamanov@itstep.edu.az>
2025-07-22 18:10:27 +0400

    Lesson 07

---
+++ MSSQL 07/MSSQL 07. Transaction.sql
@@ -0,0 +1,57 @@
+﻿-- Transactions - Tranzaksiyalar
+
+/*
+	ACID
+	 * Atomicity	(Atomar)
+	 * Consistency	(Razılıq)
+	 * Isolatinon   (İzolyasiya)
+	 * Durability   (Dayanıqlılıq)
+*/
+
+
+-- BEGIN TRANSACTION or BEGIN TRAN
+-- COMMIT
+-- ROLLBACK
+-- SAVE TRAN
+
+--CREATE PROC AddCommentMark
+--@comid AS int,
+--@userid AS int,
+--@mark AS int
+--AS
+--	BEGIN
+--		BEGIN TRAN CommentMark
+
+--		INSERT INTO CommentRating(IdUser, IdComment, Mark)
+--		VALUES(@userid, @comid, @mark)
+
+--		IF @@ERROR != 0
+--			BEGIN
+--				PRINT 'INSERT ERROR'
+--				ROLLBACK TRAN CommentMark
+--			END
+--		ELSE
+--			BEGIN
+--				PRINT 'INSERT OK'
+--				UPDATE Comments
+--				SET Rating = (	SELECT CAST(SUM(Mark) AS float) / COUNT(*)
+--								FROM Comments AS C
+--								INNER JOIN CommentRating AS CR
+--								ON C.Id = CR.IdComment
+--								WHERE C.Id = @comid)
+--				WHERE Id = @comid
+--				IF @@ERROR != 0
+--					BEGIN
+--						PRINT 'UPDATE ERROR'
+--						ROLLBACK TRAN CommentMark
+--					END
+--				ELSE
+--					BEGIN
+--						PRINT 'UPDATE OK'
+--						COMMIT TRAN CommentMark
+--					END
+--			END
+--	END
+
+
+--EXEC AddCommentMark 2, 3, 4
---
+++ MSSQL 07/MSSQL 07. UDF.sql
@@ -0,0 +1,82 @@
+﻿-- User Definition Functions - (UDF)
+
+-- UDF iki tipdə olur
+-- Scalar-valued functions
+-- Table-valued functions
+
+USE [Library]
+
+--CREATE FUNCTION CountBooksForReturn(@stud_id AS int)
+--RETURNS int
+--AS
+--BEGIN
+--	DECLARE @books AS int = 0
+--	SELECT @books = COUNT(*)
+--	FROM S_Cards
+--	WHERE S_Cards.Id_Student = @stud_id AND S_Cards.DateIn IS NULL
+
+--	RETURN @books
+--END
+
+--SELECT dbo.CountBooksForReturn(5) AS BookCount
+
+--DECLARE @count int = 0
+--EXEC @count = CountBooksForReturn 5
+--SELECT @count AS BookCount
+
+-- Table valued example
+--CREATE FUNCTION BookList()
+--RETURNS TABLE
+--AS
+--RETURN(
+--	SELECT	B.[Name] AS BookName, 
+--			C.[Name] AS CategoryName,
+--			T.[Name] AS ThemeName,
+--			A.FirstName + N' '+ A.LastName AS Author
+
+--	FROM Books AS B
+--	INNER JOIN Authors AS A ON A.Id = B.Id_Author
+--	INNER JOIN Categories AS C ON C.Id = B.Id_Category
+--	INNER JOIN Themes AS T ON T.Id = B.Id_Themes
+--)
+
+--SELECT *
+--FROM BookList()
+
+
+--CREATE FUNCTION BooksCountByAuthor()
+--RETURNS @BooksAuthors TABLE (Author nvarchar(50), BooksCount int)
+--AS
+--BEGIN 
+--	DECLARE @temp_books TABLE (Author nvarchar(50), BooksCount int)
+--	INSERT @temp_books
+--	SELECT A.FirstName + N' ' + A.LastName, COUNT(SC.Id_Book)
+--	FROM Books AS B
+--	INNER JOIN Authors AS A ON B.Id_Author = A.Id
+--	INNER JOIN S_Cards AS SC ON SC.Id_Book = B.Id
+--	GROUP BY A.FirstName, A.LastName
+
+--	INSERT @temp_books
+--	SELECT A.FirstName + N' ' + A.LastName, COUNT(TC.Id_Book)
+--	FROM Books AS B
+--	INNER JOIN Authors AS A ON B.Id_Author = A.Id
+--	INNER JOIN T_Cards AS TC ON TC.Id_Book = B.Id
+--	GROUP BY A.FirstName, A.LastName
+
+--	DECLARE @temp_books2 TABLE (Author nvarchar(50), BooksCount int)
+--	INSERT @temp_books2
+--	SELECT T.Author, SUM(T.BooksCount)
+--	FROM @temp_books AS T
+--	GROUP BY T.Author
+
+--	INSERT @BooksAuthors
+--	SELECT T.Author, T.BooksCount
+--	FROM @temp_books2 AS T
+
+--	RETURN
+--END
+
+--SELECT  *
+--FROM BooksCountByAuthor() AS B
+--WHERE BooksCount = (SELECT MAX(BooksCount)
+--					  FROM BooksCountByAuthor() AS B)
